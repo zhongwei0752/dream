29a50af9adf4c0a520791533a448fd98d1c140ee
zhongwei0752 <623610577@qq.com>
2013-04-22 15:30:39 +0800

    版本美化V1.1

--- dream/home/data/data_config.php
+++ dream/home/data/data_config.php
@@ -100,7 +100,7 @@ $_SCONFIG=Array
 			'click' => 1
 			)
 		),
-	'cronnextrun' => 1366442700,
+	'cronnextrun' => 1366615500,
 	'my_status' => '0',
 	'uniqueemail' => 1,
 	'updatestat' => 1,
---
+++ dream/home/source/space_timer.php
@@ -0,0 +1,12 @@
+<?php
+/*
+	[UCenter Home] (C) 2007-2008 Comsenz Inc.
+	$Id: space_timer.php 13206 2009-08-20 02:31:30Z liguode $
+*/
+
+if(!defined('IN_UCHOME')) {
+	exit('Access Denied');
+}
+
+include_once template("space_timer");
+?>
